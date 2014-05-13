cd302644665121afe7cf5e1a94a66db3eff8d868
davidkelley <david.james.kelley@gmail.com>
2014-05-13 22:11:21 +0100

    Wrote shared client specs

--- spec/support/client_shared.rb
+++ spec/support/client_shared.rb
@@ -1,12 +1,34 @@
 shared_examples "a client class" do
 
+  include Savon::SpecHelper
+
+  before(:all) { savon.mock! }
+  after(:all) { savon.unmock! }
+
   it { should respond_to(:request) }
   it { should respond_to(:client) }
   it { should respond_to(:namespace) }
+  it { should respond_to(:wsdl) }
   it { should respond_to(:action) }
   it { should respond_to(:operations) }
 
+  # describe "when #request is called" do
+
+  #   let(:type) { :type }
+  #   let(:opts) { { message: {} } }
+
+  #   it "should call the savon client" do
+  #     savon.expects(:call).with(type, opts)
+  #     subject.request(type, opts)
+  #   end
+
+  # end
+
   describe "return value of #namespace" do
+
+    it "should not be empty" do
+      expect(subject.namespace.endpoint).to_not be_empty
+    end
 
     it "should be a fully resolved namespace" do
       expect(subject.namespace.endpoint).to include(Reuters.namespaces_endpoint)
@@ -21,12 +43,19 @@ shared_examples "a client class" do
 
   end
 
+  describe "return value of #wsdl" do
+
+    it "should not be empty" do
+      expect(subject.wsdl.endpoint).to_not be_empty
+    end
+
+    it "should be a fully resolved wsdl" do
+      expect(subject.wsdl.endpoint).to include(Reuters.wsdl_endpoint)
+    end
+
+  end
+
   describe "return value of #client" do
-
-    include Savon::SpecHelper
-
-    before(:all) { savon.mock! }
-    after(:all) { savon.unmock! }
 
     it "should be a Savon Client" do
       expect(subject.client).to be_a(Savon::Client)

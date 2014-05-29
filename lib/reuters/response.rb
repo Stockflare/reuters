740d955bdd8a452c4941eb50432647d4734500a8
davidkelley <david.james.kelley@gmail.com>
2014-05-29 21:06:52 +0100

    Slight refactor of Response class

--- lib/reuters/response.rb
+++ lib/reuters/response.rb
@@ -10,19 +10,18 @@ module Reuters
     def initialize(body = {})
       unless body.empty?
         merge! body
-        attribs = body.keep_if { |k| k.match /@/ }
-        attribs = Hash[attribs.map { |k,v| [k.to_s.gsub(/@/,'').to_sym, v] }]
+        attribs = body.keep_if { |k| k.match(/@/) }
+        attribs = Hash[attribs.map { |k, v| [k.to_s.gsub(/@/, '').to_sym, v] }]
         @attributes = self.class.new attribs
       end
     end
 
-    def method_missing(name, *a)
+    def method_missing(name)
       if key?(name)
-        val = self[name]
-        unless val.is_a? String
-          self.class.new val
-        else
+        if (val = self[name]).is_a? String
           val
+        else
+          self.class.new val
         end
       end
     end
